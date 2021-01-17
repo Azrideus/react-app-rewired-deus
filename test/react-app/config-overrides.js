274f786fe64dc7130cfc4d99791ab727c6d07294
Mehrdad Tavangar <mehrdadtavangar@gmail.com>
2021-01-17 14:00:50 +0300

    Renamed package , fixed bugs

--- test/react-app/config-overrides.js
+++ test/react-app/config-overrides.js
@@ -1,27 +1,27 @@
 const {
   override,
-} = require('customize-cra');
+} = require('customize-cra-deus');
 
 // override
 module.exports = {
   webpack: override(
-    // customize-cra plugins here
+    // customize-cra-deus plugins here
 
     (config) => {
       return config;
-    },
+    }
   ),
 
-  jest: config => {
+  jest: (config) => {
     return config;
   },
 
-  devServer: configFunction => (proxy, allowedHost) => {
+  devServer: (configFunction) => (proxy, allowedHost) => {
     const config = configFunction(proxy, allowedHost);
     return config;
   },
 
   paths: (paths, env) => {
     return paths;
-  }
+  },
 };
